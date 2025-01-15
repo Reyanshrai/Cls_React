952884c8fd1c65c6c3ecdedf5b522712d4b2f1c7
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-15 11:30:50 +0530

    feat: add calculator component and update app structure

--- FirstReact/src/App.jsx
+++ FirstReact/src/App.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import StateMerge from './26-12-24/StateMerge'
 import Header from './26-12-24/Header'
+import SimpleCalculator from './15-1-24/Calcu'
 
 const App = () => {
   return (
     <div>
       {/* <Header></Header> */}
-      <StateMerge></StateMerge>
+      {/* <StateMerge></StateMerge> */}
+      <SimpleCalculator/>
     </div>
   )
 }
