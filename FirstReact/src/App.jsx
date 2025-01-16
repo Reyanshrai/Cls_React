7721b534c4bd97303ce87b3083f2e33df684526b
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-16 13:26:28 +0530

    refactor: update App component to use UsinguseEffect and comment out unused imports

--- FirstReact/src/App.jsx
+++ FirstReact/src/App.jsx
@@ -1,14 +1,16 @@
-import * as React from 'react'
-import StateMerge from './26-12-24/StateMerge'
-import Header from './26-12-24/Header'
-import SimpleCalculator from './15-1-24/Calcu'
+// import * as React from 'react'
+// import StateMerge from './26-12-24/StateMerge'
+// import Header from './26-12-24/Header'
+// import SimpleCalculator from './15-1-24/Calcu'
+import UsinguseEffect from './16-1-25/UseEffect'
 
 const App = () => {
   return (
     <div>
       {/* <Header></Header> */}
       {/* <StateMerge></StateMerge> */}
-      <SimpleCalculator/>
+      {/* <SimpleCalculator/> */}
+      <UsinguseEffect/>
     </div>
   )
 }
