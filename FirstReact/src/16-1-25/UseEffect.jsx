75539112cfd5ff5d99d94f48fca4f6c51d20c393
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2025-01-17 10:14:23 +0530

    feat: update ApiTesting component to fetch and display a list of flowers

--- FirstReact/src/16-1-25/UseEffect.jsx
+++ FirstReact/src/16-1-25/UseEffect.jsx
@@ -1,33 +1,107 @@
 import {useEffect, useState} from 'react'
 
-function ApiCallSimulation(){
+// function ApiCallSimulation(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve({
+//                 id:1,
+//                 name:"RKU Students"
+//             })
+//         },5000)
+//     })
+// }
+
+// function ApiTesting(){
+//     const [id,setId] = useState("Loading...")
+//     const [name , setName] = useState("Loading...")
+
+//     useEffect(() =>{
+//         ApiCallSimulation().then((user =>{
+//             setId(user.id)
+//             setName(user.name)
+//         }))
+//     },[])
+
+//     return(
+//         <div>
+//             <h1>ID : {id}</h1>
+//             <h1>Name : {name}</h1>
+//         </div>
+//     )
+// }
+
+
+
+// const [flower , setFlower] = useState([
+//     "Rose",
+//     "Tulip",
+//     "Sunflower",
+//     "Daisy",
+//     "Lily",
+//     "Orchid",
+//     "Marigold",
+//     "Chrysanthemum",
+//     "Jasmine",
+//     "Lotus",
+//     "Daffodil",
+//     "Carnation",
+//     "Hibiscus",
+//     "Lavender",
+//     "Peony",
+//     "Magnolia",
+//     "Poppy",
+//     "Iris",
+//     "Geranium",
+//     "Begonia"
+// ])
+
+function ApiCallFlower(){
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve({
-                id:1,
-                name:"RKU Students"
-            })
-        },5000)
-    })
+            resolve([
+                "Rose",
+                "Tulip",
+                "Sunflower",
+                "Daisy",
+                "Lily",
+                "Orchid",
+                "Marigold",
+                "Chrysanthemum",
+                "Jasmine",
+                "Lotus",
+                "Daffodil",
+                "Carnation",
+                "Hibiscus",
+                "Lavender",
+                "Peony",
+                "Magnolia",
+                "Poppy",
+                "Iris",
+                "Geranium",
+                "Begonia"
+            ]);
+        }, 5000);
+    });
 }
 
 function ApiTesting(){
-    const [id,setId] = useState("Loading...")
-    const [name , setName] = useState("Loading...")
+    const [flowers, setFlowers] = useState(["Loading..."]);
 
     useEffect(() =>{
-        ApiCallSimulation().then((user =>{
-            setId(user.id)
-            setName(user.name)
-        }))
-    },[])
+        ApiCallFlower().then((flowerList) =>{
+            setFlowers(flowerList);
+        });
+    }, []);
 
     return(
         <div>
-            <h1>ID : {id}</h1>
-            <h1>Name : {name}</h1>
+            <ul>
+                {flowers.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
 
-export default ApiTesting
+export default ApiTesting;
